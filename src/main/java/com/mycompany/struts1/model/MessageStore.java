d3dba8e609a4924a2b31be378e0ecc7bf5ec2cf9
Pelayo Yoler <quispeccentey@gmail.com>
2024-11-14 23:33:06 -0500

    example form

---
+++ src/main/java/com/mycompany/struts1/model/MessageStore.java
@@ -0,0 +1,20 @@
+package com.mycompany.struts1.model;
+
+public class MessageStore {
+
+    private String message;
+
+    public MessageStore() {
+        message = "PELAYO YOLER QUISPE CCENTE";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String Message) {
+        this.message = Message;
+    }
+    
+    
+}
